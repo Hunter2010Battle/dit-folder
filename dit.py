94381c487647b7f0cc98be41d59b05db73810af4
Hunter <hunterbattle11@gmail.com>
2026-03-03 14:21:09 +1300

    made a code abbout the age for licence

--- dit.py
+++ dit.py
@@ -1,5 +1,4 @@
-print('Hello')
-print('Hi there')
-print("ffgdgdf")
-print('Bye')
-print('njvjkbjkfw')
+LICENCE_AGE = 16 
+persons_age = 15
+if persons_age < LICENCE_AGE: print('You are to young to get your licence')
+if persons_age < 16: print('You are to young to get your licence')
