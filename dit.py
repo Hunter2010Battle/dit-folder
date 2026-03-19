ca2fc842bcf6dde789e5f4809c0e591fa0b36be8
Hunter2010Battle <hunterbattle11@gmail.com>
2026-03-20 08:53:59 +1300

    made list commands

--- dit.py
+++ dit.py
@@ -4,10 +4,10 @@ camp_list = ['0','1','2']
 print('These are the activities and costs')
 print('number    activity                days   difficulty    cost')
 print('0         cultural immersion      5      easy          800')
-print('1         Kayaking amd pancakes     3      moderate      400')
+print('1         Kayaking amd pancakes   3      moderate      400')
 print('2         Mountain biking         4      difficult     900')
 camper_name = ''
 camper_name = input('What is your name? ')
 if camper_name == '':
   print('You must enter you name!')
-  if camper_name == '':
+  
