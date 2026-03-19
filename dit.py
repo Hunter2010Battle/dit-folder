9d19cdb356a5307b2565c237e829b1e420b22794
Hunter2010Battle <hunterbattle11@gmail.com>
2026-03-20 09:18:27 +1300

    mae an input statement about the age

--- dit.py
+++ dit.py
@@ -6,8 +6,11 @@ print('number    activity                days   difficulty    cost')
 print('0         cultural immersion      5      easy          800')
 print('1         Kayaking amd pancakes   3      moderate      400')
 print('2         Mountain biking         4      difficult     900')
-camper_name = ''
+camper_name = 'Jack'
 camper_name = input('What is your name? ')
 if camper_name == '':
   print('You must enter you name!')
-  print("hello")
+camper_age = 5-17
+if camper_name == 'Jack': camper_age = input('what is your age? ')
+if camper_age == '':
+  print('You must enter your age!')
