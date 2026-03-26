cc1feed5b196ed57e35cfbad422ca45cfc6a4ef8
Hunter2010Battle <hunterbattle11@gmail.com>
2026-03-27 09:20:37 +1300

    print statements

--- dit.py
+++ dit.py
@@ -1,16 +1,9 @@
 #list
-camp_list = ['0','1','2']
+activity_list = ['0','Cultral Immersion','5','easy','800']
+activity_list2 = ['1','Kayaking And Pancakes','3','moderate','400']
+activity_list3 = ['2','Mountain biking','4','difficult','900']
 
 print('These are the activities and costs')
-print('number    activity                days   difficulty    cost')
-print('0         cultural immersion      5      easy          800')
-print('1         Kayaking amd pancakes   3      moderate      400')
-print('2         Mountain biking         4      difficult     900')
-camper_name = 'Jack'
-camper_name = input('What is your name? ')
-if camper_name == '':
-  print('You must enter you name!')
-camper_age = 5-17
-if camper_name == 'Jack': camper_age = input('what is your age? ')
-if camper_age == '':
-  print('You must enter your age!')
+print("number    activity                days   difficulty    cost")
+
+print(" 0        Cultural Emmersion       5     Easy           $800")
